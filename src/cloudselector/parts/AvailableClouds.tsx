ecbcba3622d07f7d8257a58881296088326fa8f9
Ilmari Kumpula <ilmari.kumpula@gmail.com>
2020-10-26 16:31:55 +0200

    Display all available clouds in a table
    No filtering done just yet

--- src/cloudselector/parts/AvailableClouds.tsx
+++ src/cloudselector/parts/AvailableClouds.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from "react";
 
 export function AvailableClouds(props) {
-    return (
-        <div>
-            Availableclouds
-        </div>
-    );
+  return (
+    <div style={{ marginTop: "10vh" }}>
+      <table>
+        <tbody>
+          {props.allClouds.map((cloud) => {
+            return (
+              <tr style={{ textAlign: "left" }}>
+                <th>{cloud["cloud_description"]}</th>
+                <th>{cloud["cloud_name"]}</th>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
