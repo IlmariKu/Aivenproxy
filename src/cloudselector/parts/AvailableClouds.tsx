96eb9cb2038564d13c098be062c124e1f30e8bea
Ilmari Kumpula <ilmari.kumpula@gmail.com>
2020-10-26 18:01:25 +0200

    Adding function to calculate distance between to lon-lat coordinates
    Answer copied from: https://stackoverflow.com/a/51819649/

--- src/cloudselector/parts/AvailableClouds.tsx
+++ src/cloudselector/parts/AvailableClouds.tsx
@@ -9,6 +9,21 @@ export function AvailableClouds(props) {
     console.log(position.coords.longitude);
   }
 
+    function distanceTo(lat1, lon1, lat2, lon2) {
+        var radlat1 = Math.PI * lat1/180
+        var radlat2 = Math.PI * lat2/180
+        var theta = lon1-lon2
+        var radtheta = Math.PI * theta/180
+        var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+        if (dist > 1) {
+            dist = 1;
+        }
+        dist = Math.acos(dist)
+        dist = dist * 180/Math.PI
+        dist = dist * 60 * 1.1515
+        return dist * 1.609344
+    }
+
   function denyLocation() {
     console.log("Ei ole mahdollista ottaa sijaintiasi.");
   }
