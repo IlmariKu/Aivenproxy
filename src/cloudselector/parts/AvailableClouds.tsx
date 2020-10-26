299bdc192cbc18cffecd9ec4654237b49670dc3a
Ilmari Kumpula <ilmari.kumpula@gmail.com>
2020-10-26 17:35:45 +0200

    Button & functionality for users Geolocation

--- src/cloudselector/parts/AvailableClouds.tsx
+++ src/cloudselector/parts/AvailableClouds.tsx
@@ -1,26 +1,41 @@
 import React from "react";
 import { getCloudProvider } from "~/src/cloudselector/utils/cloudnameparser";
 import { isEmpty } from "lodash";
+import styled from "styled-components";
 
 export function AvailableClouds(props) {
+  function successLocation(position) {
+    console.log(position.coords.latitude);
+    console.log(position.coords.longitude);
+  }
 
-    function isRegionBanned(cloudname, region){
-        const provider = getCloudProvider(cloudname)
-        const bannedRegions = props.cloudProviders[provider]["bannedRegions"]
-        if (bannedRegions.includes(region)){
-            return true
-        }
-        return false
+  function denyLocation() {
+    console.log("Ei ole mahdollista ottaa sijaintiasi.");
+  }
+
+  function getUserLocation() {
+    const geo = navigator.geolocation;
+    geo.getCurrentPosition(successLocation, denyLocation);
+  }
+
+  function isRegionBanned(cloudname, region) {
+    const provider = getCloudProvider(cloudname);
+    const bannedRegions = props.cloudProviders[provider]["bannedRegions"];
+    if (bannedRegions.includes(region)) {
+      return true;
     }
+    return false;
+  }
 
   return (
     <div style={{ marginTop: "10vh" }}>
+      <SorterButton onClick={getUserLocation}>Sort by my location</SorterButton>
       <table>
         <tbody>
           {props.allClouds.map((cloud) => {
-              if (isRegionBanned(cloud["cloud_name"], cloud["geo_region"])){
-                  return
-              }
+            if (isRegionBanned(cloud["cloud_name"], cloud["geo_region"])) {
+              return;
+            }
             return (
               <tr style={{ textAlign: "left" }}>
                 <th>{cloud["cloud_description"]}</th>
@@ -33,3 +48,10 @@ export function AvailableClouds(props) {
     </div>
   );
 }
+
+const SorterButton = styled.div`
+  width: 200px;
+  border: solid;
+  height: 100px;
+  background-color: orange;
+`;
