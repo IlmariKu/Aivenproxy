ada5a2e61f0f634d4955fbe9809e1e998b96cb50
Ilmari Kumpula <ilmari.kumpula@gmail.com>
2020-10-27 16:41:03 +0200

    Set-up CORS in a more robust way
    https://fastapi.tiangolo.com/tutorial/cors/

--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,30 @@
 import os
 import requests
+import math
+from typing import List, Dict
 
-from fastapi import FastAPI, Response
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:1234"],
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
+
+
 @app.get("/get_clouds")
-def say_hello(response: Response):
+def say_hello():
     """
     Caching is not implemented, but as a production-app,
     this proxy would cache responses, probably would use Redis
     """
     caching_enabled = os.environ.get("CACHING_ENABLED", False)
-    development = os.environ.get("DEVELOPMENT", True)
     if caching_enabled: # and result is in cache
         pass # would return a cached response
 
-    if development:
-        response.headers["Access-Control-Allow-Origin"] = "*"
-        response.headers["Access-Control-Allow-Headers"] = "Content-Type"
     res = requests.get("https://api.aiven.io/v1/clouds")
 
     return res.json()
